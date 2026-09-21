1a5cd9a5f8cdce36f2ecbcdb30fce6776924861f
chandu <chandangowdachandu550@gmail.com>
2026-09-21 23:00:13 +0530

    3D Array and Enhanced For Loop for it

--- twoDArrays.java
+++ twoDArrays.java
@@ -56,11 +56,49 @@ System.out.println("Using Enhanced for loop");
         }
         return 0;
     }
+
+    // 3D Array
+    // 3D Array is an array of 2D arrays. It is also known as a three-dimensional array.
+    //in 2d array we have rows and columns but in 3d array we have rows, columns and depth.
+    static int threeDArray(){
+        int [][][] arr = new int[3][4][5];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                for(int k=0;k<5;k++){
+                    arr[i][j][k]=(int)(Math.random()*100);
+                }
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                for(int k=0;k<5;k++){
+                    System.out.print(arr[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+// Enhanced for loop
+        System.out.println("Using Enhanced for loop");
+        for(int n[][] : arr){
+            for(int m[] : n){
+                for(int p : m){
+                    System.out.print(p+" ");
+                }
+            }
+            System.out.println();
+            }
+            
+        return 0;
+    }
     public static void main(String[] args)
     {
         // int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
         TwoDArray();
         jaggedArray();
+        threeDArray();
         
     }
 }
